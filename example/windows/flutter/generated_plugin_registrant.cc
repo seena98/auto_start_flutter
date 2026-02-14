b01a7a46668523b77b7748942150db82d42df93b
seena98 <sinaashrafpour@gmail.com>
2026-02-14 20:33:18 +0100

    Add Windows support to example app

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <auto_start_flutter/auto_start_flutter_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AutoStartFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AutoStartFlutterPlugin"));
+}
